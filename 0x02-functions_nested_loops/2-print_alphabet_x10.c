2fabae4604269cb1e5df1d3783aabf5360c71f03
MrForson <andyforson113@gmail.com>
2023-09-13 20:03:54 -0700

    poor boy

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Print the alphabet 10 times
+ * Return: Always 0
+ */
+void print_alphabet_x10(void)
+{
+int i;
+int j;
+for (j = i; i <= 10; g++)
+{
+for (j = 97; j <= 122; j++)
+{
+_putchar(g);
+}
+_putchar('\n');
+}
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+im having problem with my network

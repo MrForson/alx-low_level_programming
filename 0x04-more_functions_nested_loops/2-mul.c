715235b6cc514fcb253311c41e28535dc36bfe58
MrForson <andyforson113@gmail.com>
2023-09-15 20:59:31 -0700

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul -function checks for a digit through 0
+ * @a: a - variable
+ * @b: b - variable
+ * Return: Always 0
+ */
+int mul(int a, int b)
+{
+int mul;
+mul = a * b;
+return (mul);
+}
